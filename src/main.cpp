457df5d067152520787452a623d2ba730401a6b2
icetd <tiandengzbc@gmail.com>
2023-04-06 22:18:25 +0800

    initialize commit

---
+++ src/main.cpp
@@ -0,0 +1,104 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include "Texture.h"
+#include "log.h"
+
+int LogLevel;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
+}
+
+int main()
+{
+	initLogger(NOTICE);
+	GLFWwindow* window;
+
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	window = glfwCreateWindow(800, 600, "Renderer", NULL, NULL);
+	if (!window) {
+		glfwTerminate();
+		return -1;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSwapInterval(1);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		LOG(ERROR, "Failed to initialize GLAD");
+		return -1;
+	}
+
+	LOG(NOTICE, "%s\n", glGetString(GL_VERSION));
+
+	float position[] = {
+		-0.5f, -0.5f, 0.0f, 0.0f,
+		 0.5f, -0.5f, 1.0f, 0.0f,
+		 0.5f,  0.5f, 1.0f, 1.0f,
+		-0.5f,  0.5f, 0.0f, 1.0f
+	};
+
+	GLuint indices[] = {
+		0 , 1 , 2,
+		2 , 3 , 0
+	};
+
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC1_ALPHA);
+
+
+	/* VAO && VBO */
+	VertexArray va;
+	VertexBuffer vb(position, 4 * 4 * sizeof(float));
+	VertexBufferLayout layout;
+	layout.Push<float>(2);
+	layout.Push<float>(2);
+	va.AddBuffer(vb, layout);
+
+	/* IBO */
+	IndexBuffer ib(indices, 6);
+	
+	/* shaderer */
+	Shader shader("../res/shaders/Basic.shader");
+	shader.Bind();
+	shader.SetUniform1i("u_Texture", 1);
+
+	/* texture */
+	Texture texture("../res/textures/tiandeng.png");
+	texture.Bind(1);
+
+	va.UnBind();
+	vb.Unbind();
+	ib.Unbind();
+	shader.UnBind();
+
+	Renderer renderer;
+
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		renderer.Clear();
+		shader.Bind();
+		texture.Bind(1);
+		renderer.Draw(va, ib, shader);
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
+
+	return 0;
+}
