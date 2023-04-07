8c9133620af4dd1bb1151d8eeae4db8601f7d359
icetd <tiandengzbc@gmail.com>
2023-04-07 17:14:57 +0800

    support virtual machine

--- src/main.cpp
+++ src/main.cpp
@@ -32,8 +32,8 @@ int main()
 	/* Initialize the library */
 	if (!glfwInit())
 		return -1;
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	window = glfwCreateWindow(800, 800, "Renderer", NULL, NULL);
